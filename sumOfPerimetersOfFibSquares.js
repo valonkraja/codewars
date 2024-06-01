09e2df6f06ce91256d08f0228c51d768f7dc36ff
valonkraja <74757611+valonkraja@users.noreply.github.com>
2024-06-01 09:00:10 +0200

    Add files via upload

---
+++ sumOfPerimetersOfFibSquares.js
@@ -0,0 +1,29 @@
+/*
+
+The drawing shows 6 squares the sides of which have a length of 1, 1, 2, 3, 5, 8. It's easy to see that the sum of the perimeters of these squares is : 4 * (1 + 1 + 2 + 3 + 5 + 8) = 4 * 20 = 80 
+
+Could you give the sum of the perimeters of all the squares in a rectangle when there are n + 1 squares disposed in the same manner as in the drawing:
+
+Fibonacci sequence
+
+*/
+
+function fib(num){
+  var a = 1, b = 0, temp;
+
+  while (num >= 0){
+    temp = a;
+    a = a + b;
+    b = temp;
+    num--;
+  }
+
+  return b;
+}
+
+
+function perimeter(n) {
+    return 4 * (fib(n+2) - 1)
+}
+
+console.log(perimeter(5))
